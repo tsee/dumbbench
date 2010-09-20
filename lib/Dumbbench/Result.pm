37115f1d2189ae593b55e58146af0ad4b7f38d07
Steffen Mueller <smueller@cpan.org>
2010-09-20 16:08:07 +0200

    Fix inheritance/cloning for ::Result via Number::WithError

--- lib/Dumbbench/Result.pm
+++ lib/Dumbbench/Result.pm
@@ -16,23 +16,22 @@ use Class::XSAccessor {
 sub new {
   my $proto = shift;
   my $class = ref($proto)||$proto;
-  my %opt = @_;
+
   my $self;
-  if (not ref($proto)
-      or defined $opt{timing} || defined $opt{uncertainty} 
-         || defined $opt{nsamples})
-  {
-    if (not defined $opt{timing} or not defined $opt{uncertainty}
-        or not defined $opt{nsamples}) {
-      Carp::croak("Need 'timing', 'uncertainty', and 'nsamples' parameters");
-    }
-    $self = $class->SUPER::new($opt{timing}, $opt{uncertainty});
-    $self->{_dbr_nsamples} = $opt{nsamples};
-  }
-  else {
-    $self = $proto->SUPER::new();
+  if (not grep {$_ eq 'timing'} @_) {
+    push @_, 0 if @_ == 1;
+    $self = $proto->SUPER::new(@_);
     $self->{_dbr_nsamples} = $proto->nsamples;
+    return $self;
   }
+
+  my %opt = @_;
+  if (not defined $opt{timing} or not defined $opt{uncertainty}
+      or not defined $opt{nsamples}) {
+    Carp::croak("Need 'timing', 'uncertainty', and 'nsamples' parameters");
+  }
+  $self = $class->SUPER::new($opt{timing}, $opt{uncertainty});
+  $self->{_dbr_nsamples} = $opt{nsamples};
   
   return $self;
 }
